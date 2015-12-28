31d17b53b3e631fab7e36585f7091e85d4861b41
Nayed SAID ALI <nayed.saidali@gmail.com>
2015-12-28 18:00:53 +0100

    setup

--- app/App.js
+++ app/App.js
@@ -1,22 +1,58 @@
-var app = require('app')    // module to control application life
-var BrowserWindow = require('browser-window')   // module to create native browser window
+'use strict'
 
-var mainWindow = null   /* keep a global reference of the window object,
-                           if you don't, the window will be closed automatically
-                           when the Javascript object is garbage collected */
+const app                   = require('app')
+const browserWindow         = require('browser-window')
+const electron              = require('electron')
+const ipc                   = electron.ipcMain
+const Menu                  = require('menu')
 
-app.on('window-all-closed', () => {     // quit when all windows are closed
-    if (process.platform != 'darwin') {     /* on OSX it is common for app and their menu bar
-                                               to stay active until the user quits explicitely with
-                                               Cmd + Q */
-        app.quit()
-    }
+//-- reports any crashes to the server
+require('crash-reporter').start()
+
+
+//-- set up the main app menu
+const menu_template = require('./inc/app-menu.js')
+const main_menu = Menu.buildFromTemplate(menu_template)
+
+
+//-- create a global var for all the windows
+const appWindows = {
+  main : null
+}
+
+/**
+ * quit the app when all windows are closed
+ */
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin')
+    app.quit()
 })
 
+/**
+ * main app init
+ */
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({width: 900, height: 600})
-    mainWindow.loadURL('file://' + __dirname + '/index.html')
-    mainWindow.on('closed', () => {
-        mainWindow = null
+  //-- init the main window
+  appWindows.main = new browserWindow({
+      width: 1000,
+      height: 700,
+      frame : false
     })
+  //-- open up the dev tools for debugging
+  appWindows.main.openDevTools()
+  //apply the main menu
+  Menu.setApplicationMenu(main_menu)
+  /**
+   * unset the appWindows.main variable on close
+   */
+  appWindows.main.on('closed', () => {
+    appWindows.main = null
+  })
+
+  appWindows.main.loadURL('file://' + __dirname + '/pages/main.html')
+
+  /**
+   * listeners
+   */
+
 })
--- resources/js/pages/Main.es6
+++ resources/js/pages/Main.es6
@@ -2,5 +2,6 @@ import Vue from 'vue'
 
 export default class Main extends Vue{
     constructor() {
+        super()
     }
 }
